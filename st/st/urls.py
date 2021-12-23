bdd3b6ac9dd171ece54dc95185d64b4f44bc0989
unknown <Kaplun.O@SLM-K-0113.office.corp>
2021-12-23 16:36:16 +0200

    Testing ORM

--- st/st/urls.py
+++ st/st/urls.py
@@ -1,11 +1,16 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from sample.views import *
+from st import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('sample.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
